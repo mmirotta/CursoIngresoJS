4084d9164c9e02c3efbd90b8a7f06b6ec5fdd7d5
mmirotta <m.mirotta@gmail.com>
2016-08-25 21:10:20 -0300

    Se realiza el ejercicio 1.

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -1,17 +1,22 @@
-/*1.	Para el departamento de facturación:
-A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
-B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
-C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
-*/
-function Sumar () 
-{
-	
-}
-function Promedio () 
-{
-	
-}
-function PrecioFinal () 
-{
-	
-}
+var app = angular.module('ferreteFacturacion', []);
+
+app.controller('controlFacturacion', function($scope) {
+	$scope.PrecioUno = "";
+	$scope.PrecioDos = "";
+	$scope.PrecioTres = "";
+
+
+	$scope.Sumar=function(){
+		$scope.Resultado = parseInt($scope.PrecioUno) + parseInt($scope.PrecioDos) + parseInt($scope.PrecioTres);
+  	}
+
+  	$scope.Promedio=function(){
+  		$scope.Resultado = (parseInt($scope.PrecioUno) + parseInt($scope.PrecioDos) + parseInt($scope.PrecioTres)) / 3;
+  	}
+
+  	$scope.PrecioFinal=function(){
+  		var suma = (parseInt($scope.PrecioUno) + parseInt($scope.PrecioDos) + parseInt($scope.PrecioTres)) ;
+
+  		$scope.Resultado = suma + (suma * 0,21);
+  	}
+});
